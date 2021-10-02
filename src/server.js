07179dd775033881050ea36014ee3fca5dd1afa2
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-10-02 19:23:29 +0900

    using .env to keep security of the server

--- src/server.js
+++ src/server.js
@@ -19,11 +19,11 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({
-      mongoUrl: "mongodb://127.0.0.1:27017/wetube-clone-20210913",
+      mongoUrl: process.env.DB_URL,
     }),
   })
 );
