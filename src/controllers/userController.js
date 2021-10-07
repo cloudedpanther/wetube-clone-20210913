80e2ee2ce5f65948b3c5ac3f1ce0afa27afb215e
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-10-07 13:38:24 +0900

    user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -131,7 +131,17 @@ export const postChangePassword = async (req, res) => {
   return res.redirect("/users/logout");
 };
 
-export const see = (req, res) => res.send("See User");
+export const see = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
+  if (!user)
+    return res.status("404").render("404", { pageTitle: "User Not Found." });
+  return res.render("profile", {
+    pageTitle: user.name ? user.name : user.userName,
+    user,
+  });
+};
+
 export const logout = (req, res) => {
   req.session.destroy();
   res.redirect("/");
