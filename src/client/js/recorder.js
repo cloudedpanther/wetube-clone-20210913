9609842d02879911293dc68a8f9bc6c9ab8cb85e
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-10-14 16:57:23 +0900

    video recorder boom!

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,50 @@
+const video = document.getElementById("preview");
 const startBtn = document.getElementById("startBtn");
 
-const handleStartBtn = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: true,
-  });
+let stream;
+let recorder;
+let recordedFile;
+
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = recordedFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
 };
 
-startBtn.addEventListener("click", handleStartBtn);
+const handleStop = () => {
+  startBtn.innerText = "Download";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    recordedFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = recordedFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  video.srcObject = stream;
+  video.play();
+};
+
+init();
+
+startBtn.addEventListener("click", handleStart);
