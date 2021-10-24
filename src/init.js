17ee2c46ad2e79f9cdc8f662e372faf45ba314d8
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-10-25 02:04:20 +0900

    Heroku PORT variable

--- src/init.js
+++ src/init.js
@@ -11,4 +11,4 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () =>
   console.log(`✅ Listening on http://localhost:${PORT} 🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
